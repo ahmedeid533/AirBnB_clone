0109969a71db2940f7b036308eacd397023f007a
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-10-14 01:33:50 +0300

    AirBnb 0x011

--- console.py
+++ console.py
@@ -56,6 +56,77 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
     
+    def do_show(self, arg):
+        args = split(arg)
+        objcts_storage = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__get_class.keys():
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(args[0], args[1]) not in objcts_storage:
+            print("** no instance found **")
+        else:
+            print(objcts_storage["{}.{}".format(args[0], args[1])])
+    
+    def do_create(self, arg):
+        """Create a new class instance"""
+        args = split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__get_class:
+            print("** class doesn't exist **")
+        else:
+            print(eval(args[0])().id)
+            storage.save()
 
+    def do_destroy(self, arg):
+        """Delete a class instance"""
+        args = split(arg)
+        objcts_storage = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__get_class:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(args[0], args[1]) not in objcts_storage.keys():
+            print("** no instance found **")
+        else:
+            del objcts_storage["{}.{}".format(args[0], args[1])]
+            storage.save()
+
+    def do_update(self, arg):
+        """Update a class instance"""
+        args = split(arg)
+        objcts_storage = storage.all()
+
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] not in self.__get_class:
+            print("** class doesn't exist **")
+            return False
+        if len(args) == 1:
+            print("** instance id missing **")
+            return False
+        if "{}.{}".format(args[0], args[1]) not in objcts_storage.keys():
+            print("** no instance found **")
+            return False
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return False
+        if len(args) == 3:
+            print("** value missing **")
+            return False
+        
+        obj = objcts_storage["{}.{}".format(args[0], args[1])]
+        if args[2] in obj.__class__.__dict__.keys():
+            make_sure = type(obj.__class__.__dict__[args[2]])
+            obj.__dict__[args[2]] = make_sure(args[3])
+        else:
+            obj.__dict__[args[2]] = args[3]
+        storage.save()
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
